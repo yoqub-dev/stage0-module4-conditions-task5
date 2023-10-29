1abcbf4327dc5e8be2fa8dd95be5eef3d1b7b41c
yoqub-dev <yoqub.epam@gmail.com>
2023-10-30 03:30:04 +0500

    task9

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeap = (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+        System.out.println(isLeap ? "leap" : "not leap");
     }
 }
